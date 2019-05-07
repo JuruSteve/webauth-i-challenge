9349c3c073011e804b281e3fad12b68536f586bd
jurusteve <jstevemu17@gmail.com>
2019-05-06 23:52:03 -0400

    completed mvp

--- index.js
+++ index.js
@@ -2,11 +2,53 @@ const express = require('express')
 const helmet = require('helmet')
 const server = express()
 const usersRouter = require('./router/users-router')
+const Users = require('./models/users-model')
+const bcrypt = require('bcrypt')
 
 server.use(express.json())
 server.use(helmet())
+server.use('/api/register', usersRouter)
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Logged In`, cookie: user.id })
+      } else {
+        res.status(401).json({ message: `Invalid Credentials` })
+      }
+    })
+})
+server.get('/api/users', protectedRoute, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(e => {
+      res.status(400).json(e)
+    })
+})
 
-server.use('/api/users', usersRouter)
+function protectedRoute (req, res, next) {
+  const { username, password } = req.body
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next()
+        } else {
+          res.status(401).json({ message: `Invalid Credentials` })
+        }
+      })
+      .catch(e => {
+        res.status(500).json({ message: 'You shall not pass [' })
+      })
+  } else {
+    res.status(400).json({ message: 'Please provide credentials [' })
+  }
+}
 
 const port = 3300
 server.listen(port, function () {
--- router/users-router.js
+++ router/users-router.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const Dishes = require('../models/users-model')
+const Users = require('../models/users-model')
+const bcrypt = require('bcrypt')
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
+//   try {
+//     const users = await Dishes.getUsers()
+//     res.status(200).json(users)
+//   } catch (error) {
+//     res.status(500).json({ message: 'We ran into an error retrieving the dishes' })
+//   }
+// })
+
+router.post('/', async (req, res) => {
   try {
-    const users = await Dishes.getUsers()
-    res.status(200).json(users)
+    const { username, password } = req.body
+    if (!username || !password) {
+      res.status(500).json({ message: 'Please provide credentials' })
+    } else {
+      const hash = bcrypt.hashSync(password, 10)
+      const newUser = { username, password: hash }
+      const users = await Users.addUser(newUser)
+      res.status(200).json(users)
+    }
   } catch (error) {
-    res.status(500).json({ message: 'We ran into an error retrieving the dishes' })
+    res.status(500).json({ message: error })
   }
 })
 
